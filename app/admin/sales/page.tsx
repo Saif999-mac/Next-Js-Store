82e1f9cde99ad300b2741d58000441b7110e5e48
Saif Ahmed <saifahmedprince001@gmail.com>
2026-04-09 20:26:48 +0530

    Admin Restriction done

---
+++ app/admin/sales/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function salesPage() {
+  return <div></div>;
+}
+
+export default salesPage;
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -14,7 +14,7 @@ function Navbar() {
         <Suspense>
           <NavSearch />
         </Suspense>
-        <div className="flex gap-4 items-center">
+        <div className="flex gap-4 items-center ">
           <CartButton />
           <DarkMode />
           <LinksDropdown />
