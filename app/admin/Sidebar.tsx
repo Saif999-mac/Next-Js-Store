efa0c737e9cb275052078f3e66478e4c3ab96407
Saif Ahmed <saifahmedprince001@gmail.com>
2026-04-09 20:58:35 +0530

    sidebar key

--- app/admin/Sidebar.tsx
+++ app/admin/Sidebar.tsx
@@ -15,6 +15,7 @@ function Sidebar() {
         const variant = isActivePage ? "default" : "ghost";
         return (
           <Button
+            key={link.href}
             asChild
             variant={variant}
             className="w-full mb-2 capitalize font-normal justify-start"
